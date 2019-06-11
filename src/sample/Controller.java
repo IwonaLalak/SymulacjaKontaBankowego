0b8c800f8d1fe51d6d21138ab3511d00097df495
iwona Lalak <lalak.iwona@gmail.com>
2019-06-11 15:40:57 +0200

    firmowe

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -54,10 +54,45 @@ public class Controller {
     public TableColumn<Operacja, String> colOsobisteOpis;
 
 
+    /*firmowe*/
+    @FXML
+    private Label saldoFirmowe;
+
+    @FXML
+    private TextField inputFirmowePrzelewOdbiorca;
+    @FXML
+    private DatePicker inputFirmowePrzelewData;
+    @FXML
+    private TextField inputFirmowePrzelewOpis;
+    @FXML
+    private TextField inputFirmowePrzelewKwota;
+
+    @FXML
+    private TextField inputFirmowePrzelewPrzychodzacy;
+    @FXML
+    private TextField inputFirmoweWyplata;
+
+    @FXML
+    public TableView<Operacja> tableFirmowe;
+    @FXML
+    public TableColumn<Operacja, String> colFirmoweData;
+    @FXML
+    public TableColumn<Operacja, String> colFirmoweTyp;
+    @FXML
+    public TableColumn<Operacja, String> colFirmoweNadawca;
+    @FXML
+    public TableColumn<Operacja, Double> colFirmoweKwota;
+    @FXML
+    public TableColumn<Operacja, String> colFirmoweOpis;
+
+
 
     public void odswiezSaldoKonta(E_KontoType konto) {
         if (konto.equals(E_KontoType.OSOBISTE_KONTO)) {
             saldoOsobiste.setText(Double.toString(bankomatFacade.zwrocSaldo(E_KontoType.OSOBISTE_KONTO)) + " zł");
+        }
+        else if(konto.equals(E_KontoType.FIRMOWE_KONTO)){
+            saldoFirmowe.setText(Double.toString(bankomatFacade.zwrocSaldo(E_KontoType.FIRMOWE_KONTO)) + " zł");
         }
     }
 
@@ -77,6 +112,20 @@ public class Controller {
 
             tableOsobiste.setItems(data);
         }
+        else if (konto.equals(E_KontoType.FIRMOWE_KONTO)){
+            final ObservableList<Operacja> data = FXCollections.observableArrayList();
+
+            ArrayList<Operacja> operacje = bankomatFacade.zwrocListeOperacji(konto);
+            data.addAll(operacje);
+
+            colFirmoweData.setCellValueFactory(new PropertyValueFactory<Operacja, String>("data"));
+            colFirmoweTyp.setCellValueFactory(new PropertyValueFactory<Operacja, String>("typ"));
+            colFirmoweNadawca.setCellValueFactory(new PropertyValueFactory<Operacja, String>("nadawca"));
+            colFirmoweOpis.setCellValueFactory(new PropertyValueFactory<Operacja, String>("opis"));
+            colFirmoweKwota.setCellValueFactory(new PropertyValueFactory<Operacja, Double>("kwota"));
+
+            tableFirmowe.setItems(data);
+        }
     }
 
     public void showAlertAboutInput(){
@@ -85,6 +134,8 @@ public class Controller {
         alert.setHeaderText("Proszę uzupełnić wymagane dane");
         alert.showAndWait();
     }
+
+    /*osobiste*/
 
     public void onClickPrzelejNaOsobiste() {
         if (inputOsobistePrzelewPrzychodzacy.getText().length() > 0) {
@@ -132,6 +183,55 @@ public class Controller {
         } else this.showAlertAboutInput();
 
     }
+
+    /*firmowe*/
+
+    public void onClickWykonajPrzelewZFirmowego(){
+        if (inputFirmowePrzelewKwota.getText().length() > 0) {
+            double kwota = Double.parseDouble(inputFirmowePrzelewKwota.getText().replace(",", "."));
+            String odbiorca = inputFirmowePrzelewOdbiorca.getText();
+            String opis = inputFirmowePrzelewOpis.getText();
+            String data = "";
+
+            if (kwota > 0 && odbiorca.length() > 0) {
+
+                if(inputFirmowePrzelewData.getValue()!=null){
+                    data = inputFirmowePrzelewData.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                }else{
+                    data = Main.returnTodayDate();
+                }
+
+                bankomatFacade.zrobPrzelew(E_KontoType.FIRMOWE_KONTO, kwota, data, odbiorca, opis);
+                this.odswiezSaldoKonta(E_KontoType.FIRMOWE_KONTO);
+                this.odswiezOperacje(E_KontoType.FIRMOWE_KONTO);
+            } else this.showAlertAboutInput();
+        } else this.showAlertAboutInput();
+    }
+
+    public void onClickWplacZFirmowego(){
+        if (inputFirmoweWyplata.getText().length() > 0) {
+            double kwota = Double.parseDouble(inputFirmoweWyplata.getText().replace(",", "."));
+            if (kwota > 0) {
+                bankomatFacade.wyplac(E_KontoType.FIRMOWE_KONTO, kwota);
+                this.odswiezSaldoKonta(E_KontoType.FIRMOWE_KONTO);
+                this.odswiezOperacje(E_KontoType.FIRMOWE_KONTO);
+            }
+        } else this.showAlertAboutInput();
+    }
+
+    public void onClickPrzelejNaFirmowe(){
+        if (inputFirmowePrzelewPrzychodzacy.getText().length() > 0) {
+            double kwota = Double.parseDouble(inputFirmowePrzelewPrzychodzacy.getText().replace(",", "."));
+
+            if (kwota > 0) {
+                bankomatFacade.wplac(E_KontoType.FIRMOWE_KONTO, kwota);
+                this.odswiezSaldoKonta(E_KontoType.FIRMOWE_KONTO);
+                this.odswiezOperacje(E_KontoType.FIRMOWE_KONTO);
+            }
+        } else this.showAlertAboutInput();
+    }
+
+
 
 
 }
